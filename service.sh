be4f5020d75b9174d40f6c45a3dcfb1b2b81ab37
Pager <kov0583@gmail.com>
2022-02-10 16:21:02 +0700

    Добавил мониторинг сервиса php

--- service.sh
+++ service.sh
@@ -22,5 +22,18 @@ nginxstatus=$(systemctl status nginx | grep -Eo "running|dead|failed")
             systemctl restart nginx # перезапуск nginx
             sleep 1 #ожидаем 1 секунду
             echo -en "${WHITE} ${BGGREEN} Статус nginx после перезапуска $(systemctl status nginx | grep -Eo "running|dead|faoled") $(NORMAL)\n"
-            curl -I 192.168.1.156 | grep OK # Опрос сервиса nginx
+            echo $(curl -I 192.168.1.156 | grep OK) # Опрос сервиса nginx
+    fi
+# получаем статус php через переменную и записываем его в переменную
+phpfpmstatus='systemctl status php7.2-fpm | grep -Eo "running|dead|failed"'
+
+    if [[$phpfpmstatus = 'running']]
+        then
+            echo -en  "${WHITE} ${BGGREEN} Веб сервер работает ${NORMAL}\n"
+        else
+            echo -en "${WHITE} ${BGRED} php7.2-fpm не работает ${NORMAL}\n"
+            systemctl restart php7.2-fpm # перезапуск php7.2-fpm
+            sleep 1 #ожидаем 1 секунду
+            echo -en "${WHITE} ${BGGREEN} Статус nginx после перезапуска $(systemctl status php7.2-fpm | grep -Eo "running|dead|faoled") $(NORMAL)\n"
+            echo $(curl -I 192.168.1.156 | grep OK) # Опрос сервиса nginx
     fi
